74828fa7567a9ec6c4b72c75b7cc47736a342429
allicen <46901956+allicen@users.noreply.github.com>
2019-10-08 02:55:25 +0500

    Мелкие правки

--- Плагин заказа обратного звонка/ordercall/lib/config/plugin.php
+++ Плагин заказа обратного звонка/ordercall/lib/config/plugin.php
@@ -1,10 +1,10 @@
 <?php
 return array(
-    'name' => 'Заказ обратного звонка',
-    'img'  => 'img/phone.png',
-	'description'	=> 'Плагин для обратного звонка',
-	'version'	=>	'1.0',
-	'frontend'	=>	true,
+    	'name' => 'Заказ обратного звонка',
+    	'img'  => 'img/phone.png',
+	'description' => 'Плагин для обратного звонка',
+	'version' => '1.0',
+	'frontend' => true,
 	'handlers' => array(
         'frontend_head' => 'frontendHead',
     ),
